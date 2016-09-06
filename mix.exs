4fb5a2c566de36b57e80fc34a6d8cb576f889fb4
Steven Blowers <sblowers@findmypast.com>
2016-09-06 13:40:56 +0100

    upping hex version #

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Zendex.Mixfile do
 
   def project do
     [app: :zendex,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.3",
      description: description(),
      build_embedded: Mix.env == :prod,
