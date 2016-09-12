f8df0aaecd89a42ee3c73d7b05f02ff2206fb084
Steven Blowers <sblowers@findmypast.com>
2016-09-12 09:44:10 +0100

    More tests.

---
+++ lib/http_client/in_memory.ex
@@ -0,0 +1,21 @@
+defmodule Zendex.HttpClient.InMemory do
+  @moduledoc """
+  Allows testing of the Zendex project by mocking out calls to an actual Zendesk
+  API.
+  """
+
+  def get!("http://test.zendesk.com/api/v2/tickets.json",
+           [{"Authorization", _authentication}]) do
+    "ticket"
+  end
+
+  def get!("http://test.zendesk.com/api/v2/search.json?query=requester%3AJimbob+type%3Aticket",
+           [{"Authorization", _authentication}]) do
+    ["Jimbob Ticket 1", "Jimbob Ticket 2"]
+  end
+
+  def post!("http://test.zendesk.com/api/v2/tickets.json",
+            "{\"ticket\": {}}",
+            [{"Authorization", _authentication}, {"Content-Type", "application/json"} ]) do
+  end
+end
