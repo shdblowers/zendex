05209545148e094a8ae92b7206a0962db114d060
Steven Blowers <sblowers@findmypast.com>
2016-09-16 16:34:01 +0100

    adding functionality to create a user + tests

--- lib/http_client/in_memory.ex
+++ lib/http_client/in_memory.ex
@@ -4,23 +4,29 @@ defmodule Zendex.HttpClient.InMemory do
   API.
   """
 
-  def get!("http://test.zendesk.com/api/v2/tickets.json",
+  @base_url "http://test.zendesk.com"
+
+  def get!(@base_url <> "/api/v2/tickets.json",
            [{"Authorization", _authentication}]) do
     fake_response("ticket")
   end
 
-  def get!("http://test.zendesk.com/api/v2/search.json?query=requester%3AJimbob+type%3Aticket",
+  def get!(@base_url <> "/api/v2/search.json?query=requester%3AJimbob+type%3Aticket",
            [{"Authorization", _authentication}]) do
     fake_response(["Jimbob Ticket 1", "Jimbob Ticket 2"])
   end
 
-  def get!("http://test.zendesk.com/api/v2/search.json?query=requester%3AReginald+type%3Aticket&sort_by=created_at&sort_order=desc",
+  def get!(@base_url <> "/api/v2/search.json?query=requester%3AReginald+type%3Aticket&sort_by=created_at&sort_order=desc",
            [{"Authorization", _authentication}]) do
     fake_response(["Reginald Ticket 1", "Reginald Ticket 2"])
   end
 
-  def post!("http://test.zendesk.com/api/v2/tickets.json", "{\"ticket\":{}}", [{"Authorization", _authentication}, {"Content-Type", "application/json"}]) do
+  def post!(@base_url <> "/api/v2/tickets.json", "{\"ticket\":{}}", [{"Authorization", _authentication}, {"Content-Type", "application/json"}]) do
     fake_response("Ticket created successfully!")
+  end
+
+  def post!(@base_url <> "/api/v2/users.json", "{\"user\":{\"name\":\"Roger\",\"email\":\"roger@dodger.com\"}}", [{"Authorization", _authentication}, {"Content-Type", "application/json"}]) do
+    fake_response(%{user: %{id: 1234, name: "Roger", email: "roger@dodger.com"}})
   end
 
   defp fake_response(body), do: %{body: Poison.encode!(body)}
