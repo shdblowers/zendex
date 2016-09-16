b06ef211f3cc6dcad903f408ae8cc5218e1713a0
Steven Blowers <sblowers@findmypast.com>
2016-09-16 15:35:29 +0100

    piping uri creation

--- lib/zendex/search.ex
+++ lib/zendex/search.ex
@@ -14,9 +14,10 @@ defmodule Zendex.Search do
     search_string = create_search_string(query)
     sort_string = create_sort_string(sort_by, sort_order)
 
-    full_uri = connection.base_url <> @url <> search_string <> sort_string
-
-    full_uri
+    connection.base_url
+    |> Kernel.<>(@url)
+    |> Kernel.<>(search_string)
+    |> Kernel.<>(sort_string)
     |> @http_client.get!(CommonHelpers.get_headers(connection.authentication))
     |> CommonHelpers.decode_response
   end
