dcd0050be5fc05d0460c5cc6508808ba8ab1cc7e
Steven Blowers <sblowers@findmypast.com>
2016-11-01 12:42:12 +0000

    adding exact return to in memory show user route

--- lib/http_client/in_memory.ex
+++ lib/http_client/in_memory.ex
@@ -26,9 +26,42 @@ defmodule Zendex.HttpClient.InMemory do
     fake_response("users")
   end
 
-  def get!("#{@base_url}/api/v2/users/87.json",
+  def get!("#{@base_url}/api/v2/users/295204.json",
            [{"Authorization", _authentication}]) do
-    fake_response(%{"user": %{"id": 87, "name": "Quim Stroud"}})
+    fake_response(%{"user" => %{"ticket_restriction" => nil,
+                                "chat_only" => false,
+                                "shared_phone_number" => nil,
+                                "notes" => "",
+                                "phone" => nil,
+                                "organization_id" => 11129520411,
+                                "last_login_at" => "2016-10-28T21:08:23Z",
+                                "moderator" => true,
+                                "shared" => false,
+                                "id" => 295204,
+                                "role" => "admin",
+                                "external_id" => nil,
+                                "shared_agent" => false,
+                                "photo" => nil,
+                                "verified" => true,
+                                "active" => true,
+                                "locale_id" => 1,
+                                "suspended" => false,
+                                "created_at" => "2015-05-28T09:12:45Z",
+                                "name" => "Nikolao Aikema",
+                                "restricted_agent" => false,
+                                "locale" => "en-US",
+                                "details" => "",
+                                "alias" => nil,
+                                "url" => "https://test.zendesk.com/api/v2/users/295204.json",
+                                "custom_role_id" => nil,
+                                "email" => "nikolao.aikema@test.com",
+                                "signature" => nil,
+                                "two_factor_auth_enabled" => nil,
+                                "time_zone" => "London",
+                                "only_private_comments" => false,
+                                "user_fields" => %{"customer_complaint" => nil},
+                                "tags" => [],
+                                "updated_at" => "2016-10-28T21:08:23Z"}})
   end
 
   def get!("#{@base_url}/api/v2/users/show_many.json?ids=6,67",
--- test/zendex/user_test.exs
+++ test/zendex/user_test.exs
@@ -13,8 +13,41 @@ defmodule Zendex.UserTest do
   end
 
   test "showing a user", context do
-    expected = %{"user" => %{"id" => 87, "name" => "Quim Stroud"}}
-    actual = Zendex.User.show(context[:conn], 87)
+    expected = %{"user" => %{"ticket_restriction" => nil,
+                             "chat_only" => false,
+                             "shared_phone_number" => nil,
+                             "notes" => "",
+                             "phone" => nil,
+                             "organization_id" => 11129520411,
+                             "last_login_at" => "2016-10-28T21:08:23Z",
+                             "moderator" => true,
+                             "shared" => false,
+                             "id" => 295204,
+                             "role" => "admin",
+                             "external_id" => nil,
+                             "shared_agent" => false,
+                             "photo" => nil,
+                             "verified" => true,
+                             "active" => true,
+                             "locale_id" => 1,
+                             "suspended" => false,
+                             "created_at" => "2015-05-28T09:12:45Z",
+                             "name" => "Nikolao Aikema",
+                             "restricted_agent" => false,
+                             "locale" => "en-US",
+                             "details" => "",
+                             "alias" => nil,
+                             "url" => "https://test.zendesk.com/api/v2/users/295204.json",
+                             "custom_role_id" => nil,
+                             "email" => "nikolao.aikema@test.com",
+                             "signature" => nil,
+                             "two_factor_auth_enabled" => nil,
+                             "time_zone" => "London",
+                             "only_private_comments" => false,
+                             "user_fields" => %{"customer_complaint" => nil},
+                             "tags" => [],
+                             "updated_at" => "2016-10-28T21:08:23Z"}}
+    actual = Zendex.User.show(context[:conn], 295204)
 
     assert expected == actual
   end
