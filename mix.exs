1f6773180528f1f7eb9aec881d4b911964558e50
kballou <kballou@devnulllabs.io>
2016-11-11 14:45:47 -0700

    adding `exvcr` testing library dependency

--- mix.exs
+++ mix.exs
@@ -28,7 +28,8 @@ defmodule Zendex.Mixfile do
      {:credo, "~> 0.4", only: :dev},
      {:dialyxir, "~> 0.3", only: :dev},
      {:excoveralls, "~> 0.5", only: :test},
-     {:meck, "~> 0.8", only: :test}]
+     {:meck, "~> 0.8", only: :test},
+     {:exvcr, "~> 0.8.4", only: :test}]
   end
 
   defp description do
