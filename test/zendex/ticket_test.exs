c680de5bf01c0dd313701de5be5be633ed7f41c4
Steven Blowers <shdblowers@gmail.com>
2016-11-22 14:57:39 +0000

    improvements to using meck in tests

--- test/zendex/ticket_test.exs
+++ test/zendex/ticket_test.exs
@@ -1,38 +1,38 @@
 defmodule Zendex.TicketTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   setup do
-    [conn: Zendex.Connection.setup("http://test.zendesk.com", "User", "Passw")]
+    %{conn: Zendex.Connection.setup("http://test.zendesk.com",
+                                    "User",
+                                    "Password123!")}
   end
 
   setup_all do
     :meck.new(HTTPoison)
-
-    on_exit fn ->
-      :meck.unload
-    end
-
+    on_exit fn -> :meck.unload end
     :ok
   end
 
-  test "getting tickets", context do
-    :meck.expect(HTTPoison,
-                 :get!,
-                 fn("http://test.zendesk.com/api/v2/tickets.json", [{"Authorization", "Basic VXNlcjpQYXNzdw=="}]) ->
-                   %HTTPoison.Response{body: Poison.encode!("ticket")}
-                 end)
+  test "getting tickets", %{conn: conn} do
+    stub = fn("http://test.zendesk.com/api/v2/tickets.json",
+              [{"Authorization", "Basic VXNlcjpQYXNzd29yZDEyMyE="}]) ->
+      %HTTPoison.Response{body: Poison.encode!("ticket")}
+    end
+    :meck.expect(HTTPoison, :get!, stub)
 
-    assert "ticket" == Zendex.Ticket.list(context[:conn])
+    assert "ticket" == Zendex.Ticket.list(conn)
   end
 
-  test "creating a ticket", context do
-    :meck.expect(HTTPoison,
-                 :post!,
-                 fn(_, _, _) ->
-                   %HTTPoison.Response{body: Poison.encode!("Ticket created successfully!")}
-                 end)
+  test "creating a ticket", %{conn: conn} do
+    stub = fn("http://test.zendesk.com/api/v2/tickets.json",
+              "{\"ticket\":{\"title\":\"HELP!\"}}",
+              [{"Authorization", "Basic VXNlcjpQYXNzd29yZDEyMyE="},
+               {"Content-Type", "application/json"}]) ->
+      %HTTPoison.Response{body: Poison.encode!("Ticket created successfully!")}
+    end
+    :meck.expect(HTTPoison, :post!, stub)
 
     assert "Ticket created successfully!" ==
-      Zendex.Ticket.create(context[:conn], %{"ticket": %{}})
+      Zendex.Ticket.create(conn, %{"ticket": %{"title": "HELP!"}})
   end
 end
